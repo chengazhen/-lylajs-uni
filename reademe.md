bfb63085c09a3556ecbc95a8cabb970727faebd5
chengchongzhen <chengchongzhen@hexfuture.net>
2024-01-04 14:20:43 +0800

    feat: uniapp 请求

---
+++ reademe.md
@@ -0,0 +1,3 @@
+文档地址
+
+https://github.com/07akioni/lyla/blob/main/README.zh_CN.md
---
+++ src/adapter.ts
@@ -0,0 +1,71 @@
+import type {
+  LylaAdapter,
+  LylaAdapterMeta as LylaCoreAdapterMeta
+} from '@lylajs/core'
+import { headersKeyToLowerCase } from '@lylajs/core'
+import type {
+  NetworkErrorDetail,
+  ResponseDetail,
+  UniRequest,
+  UniRequestMethod
+} from './types'
+
+declare const uni: {
+  request: UniRequest
+}
+
+export interface LylaAdapterMeta extends LylaCoreAdapterMeta {
+  method: UniRequestMethod
+  networkErrorDetail: NetworkErrorDetail
+  responseDetail: ResponseDetail
+  responseType: 'arraybuffer' | 'text'
+  requestBody: string | ArrayBuffer
+  progressDetail: never
+  originalRequest: never
+}
+
+export const adapter: LylaAdapter<LylaAdapterMeta> = ({
+  url,
+  method,
+  headers,
+  body,
+  responseType,
+  onResponse,
+  onNetworkError
+  // Not used, just leave it here
+  // json,
+  // withCredentials,
+  // onDownloadProgress,
+  // onUploadProgress,
+}): {
+  abort: () => void
+} => {
+  const requestTask = uni.request({
+    url,
+    method,
+    header: headers,
+    data: body,
+    responseType,
+    // https://developers.weixin.qq.com/miniprogram/dev/api/network/request/wx.request.html
+    // Docs said if it's not json, response data won't be transformed to json.
+    dataType: 'text',
+    fail(res) {
+      onNetworkError(res)
+    },
+    success(res) {
+      onResponse(
+        {
+          body: res.data as string | ArrayBuffer,
+          status: res.statusCode,
+          headers: headersKeyToLowerCase(res.header)
+        },
+        res
+      )
+    }
+  })
+  return {
+    abort() {
+      requestTask.abort()
+    }
+  }
+}
